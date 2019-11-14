d07ea73637fc6199fe39c1c567b9d6c58f9b8ea7
rangsiya tunmoon <nameterzaza55@hotmail.com>
2019-11-14 22:55:20 +0700

    register login profile

--- mis-app/src/app/profile/profile.page.ts
+++ mis-app/src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CallapiService } from "src/app/service/callapi.service";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,150 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  DataDoctor = {
+    "dr_id": null,
+    "dr_article": null,
+    "dr_name": null,
+    "dr_sex": null,
+    "dr_user": null,
+    "dr_passwd": null,
+    "dr_tell": null,
+    "dr_address": null
+  }
+
+  DataPatient = {
+    "p_id": null,
+    "p_article": null,
+    "p_name": null,
+    "p_user": null,
+    "p_passwd": null,
+    "p_birthDate": null,
+    "p_sex": null,
+    "p_weight": null,
+    "p_height": null,
+    "p_tell": null,
+    "p_address": null
+  }
+
+  DataNurse = {
+    "nurse_id": null,
+    "nurse_article": null,
+    "nurse_name": null,
+    "nurse_sex": null,
+    "nurse_user": null,
+    "nurse_passwd": null,
+    "nurse_tell": null,
+    "nurse_address": null
+  }
+
+  DataAdmin = {
+    "admin_id": null,
+    "admin_article": null,
+    "admin_name": null,
+    "admin_sex": null,
+    "admin_user": null,
+    "admin_passwd": null,
+    "admin_tell": null,
+    "admin_address": null
+  }
+
+  dataAdmin: any = {};
+  dataDoctor: any;
+  dataNurse: any = {};
+  dataPatient: any = {};
+  checkStatus: any;
+  isValid: boolean;
+  buttonStatus: any;
+
+  constructor(public activate: ActivatedRoute, public callApi: CallapiService) {
+    console.log(this.callApi.getid);
+    this.checkStatus = this.callApi.getStatus;
+    if (this.checkStatus == "doctor") {
+      this.DataDoctor.dr_id = this.callApi.getid;
+      this.checkStatus = 1;
+      this.getProfileDoctorById();
+    }
+    else if (this.checkStatus == "admin") {
+      this.DataAdmin.admin_id = this.callApi.getid;
+      this.checkStatus = 2;
+      this.getProfileAdminById();
+    }
+    else if (this.checkStatus == "nurse") {
+      this.DataNurse.nurse_id = this.callApi.getid;
+      this.checkStatus = 3;
+      this.getProfileNurseById();
+    }
+    else if (this.checkStatus == "patient") {
+      this.DataPatient.p_id = this.callApi.getid;
+      this.checkStatus = 4;
+      this.getProfilePatientById();
+    }
+    this.buttonStatus = "1";
+    this.isValid = true;
+    console.log(this.checkStatus);
+
+  }
+
+
 
   ngOnInit() {
+ 
   }
+
+  change() {
+    if (this.buttonStatus == "1") {
+      this.isValid = false;
+      this.buttonStatus = "2";
+
+    } else if (this.buttonStatus == "2") {
+      this.isValid = true;
+      this.buttonStatus = "1";
+ 
+    } 
+    console.log(this.isValid);
+    console.log(this.buttonStatus);
+    
+  }
+
+  getProfileDoctorById() {
+    let dataForm = new FormData();
+    dataForm.append("_Data", JSON.stringify(this.DataDoctor));
+    dataForm.append("Function_Name", "getProfileDoctorById");
+    this.callApi.profile(dataForm).then((result) => {
+      this.dataDoctor = result[0];
+      console.log(this.dataDoctor);
+    });
+  }
+
+  getProfileAdminById() {
+    let dataForm = new FormData();
+    dataForm.append("_Data", JSON.stringify(this.DataAdmin));
+    dataForm.append("Function_Name", "getProfileAdminById");
+    this.callApi.profile(dataForm).then((result) => {
+      this.dataAdmin = result[0];
+      console.log(this.dataAdmin);
+    });
+  }
+
+  getProfileNurseById() {
+    let dataForm = new FormData();
+    dataForm.append("_Data", JSON.stringify(this.DataNurse));
+    dataForm.append("Function_Name", "getProfileNurseById");
+    this.callApi.profile(dataForm).then((result) => {
+      this.dataNurse = result[0];
+      console.log(this.dataNurse);
+    });
+  }
+
+  getProfilePatientById() {
+    let dataForm = new FormData();
+    dataForm.append("_Data", JSON.stringify(this.DataPatient));
+    dataForm.append("Function_Name", "getProfilePatientById");
+    this.callApi.profile(dataForm).then((result) => {
+      this.dataPatient = result[0];
+      console.log(this.dataPatient);
+    });
+  }
+
 
 }
--- mis-app/src/app/register/register.page.ts
+++ mis-app/src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CallapiService } from "src/app/service/callapi.service";
+import { NavController, AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,183 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  DataDoctor={
+    "dr_id": "",
+    "dr_article": "",
+    "dr_name": "",
+    "dr_sex": "",
+    "dr_user": "",
+    "dr_passwd": "",
+    "dr_tell": "",
+    "dr_address": ""
+  }
+
+  DataPatient={
+    "p_id": "",
+    "p_article": "",
+    "p_name": "",
+    "p_user": "",
+    "p_passwd": "",
+    "p_birthDate": "",
+    "p_sex": "",
+    "p_weight": "",
+    "p_height": "",
+    "p_tell": "",
+    "p_address": ""
+  }
+
+  DataNurse={
+    "nurse_id": "",
+    "nurse_article": "",
+    "nurse_name": "",
+    "nurse_sex": "",
+    "nurse_user": "",
+    "nurse_passwd": "",
+    "nurse_tell": "",
+    "nurse_address": ""
+  }
+
+  DataAdmin={
+    "admin_id": "",
+    "admin_article": "",
+    "admin_name": "",
+    "admin_sex": "",
+    "admin_user": "",
+    "admin_passwd": "",
+    "admin_tell": "",
+    "admin_address": ""
+  }
+
+ 
+  
+  constructor(public callApi:CallapiService ,public alertController:AlertController) { }
 
   ngOnInit() {
   }
+  async addDataDoctor() {
+    const alert = await this.alertController.create({
+      header: 'แจ้งเตือน',
+      message: 'คุณต้องการเพิ่มข้อมูลหรือไม่',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          
+        }, {
+          text: 'ตกลง',
+          handler: () => {
+
+            console.log(this.DataDoctor);
+            let dataFrom = new FormData();
+            dataFrom.append("_Data", JSON.stringify(this.DataDoctor));
+            dataFrom.append("Function_Name", "addDoctor");
+            console.log(dataFrom);
+            this.callApi.Doctor(dataFrom).then((res) => {
+              console.log(res);
+              
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async addDataAdmin() {
+    const alert = await this.alertController.create({
+      header: 'แจ้งเตือน',
+      message: 'คุณต้องการเพิ่มข้อมูลหรือไม่',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          
+        }, {
+          text: 'ตกลง',
+          handler: () => {
+
+            console.log(this.DataAdmin);
+            let dataFrom = new FormData();
+            dataFrom.append("_Data", JSON.stringify(this.DataAdmin));
+            dataFrom.append("Function_Name", "addAdmin");
+            console.log(dataFrom);
+            this.callApi.Admin(dataFrom).then((res) => {
+              console.log(res);
+              
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async addDataNurse() {
+    const alert = await this.alertController.create({
+      header: 'แจ้งเตือน',
+      message: 'คุณต้องการเพิ่มข้อมูลหรือไม่',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          
+        }, {
+          text: 'ตกลง',
+          handler: () => {
+
+            console.log(this.DataNurse);
+            let dataFrom = new FormData();
+            dataFrom.append("_Data", JSON.stringify(this.DataNurse));
+            dataFrom.append("Function_Name", "addNurse");
+            console.log(dataFrom);
+            this.callApi.Nurse(dataFrom).then((res) => {
+              console.log(res);
+              
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async addDataPatient() {
+    const alert = await this.alertController.create({
+      header: 'แจ้งเตือน',
+      message: 'คุณต้องการเพิ่มข้อมูลหรือไม่',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          
+        }, {
+          text: 'ตกลง',
+          handler: () => {
+
+            console.log(this.DataPatient);
+            let dataFrom = new FormData();
+            dataFrom.append("_Data", JSON.stringify(this.DataPatient));
+            dataFrom.append("Function_Name", "addPatient");
+            console.log(dataFrom);
+            this.callApi.Patient(dataFrom).then((res) => {
+              console.log(res);
+              
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
 
 }
